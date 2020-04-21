5c6e7bdfb9517c20c80f68f51c7ded96ee199172
RihardsJ <rihards.jukna@gmail.com>
2020-04-21 23:52:27 +0100

    final adjustments to carousel
    removed shadows areound the main container
    add flip animation
    adjusted borders and smoothen edges for the carousel
    add reset functionality to improve user interaction and avoid sudden image change during manual image change

--- carousel.js
+++ carousel.js
@@ -1,7 +1,7 @@
 
 //assign variables to carousel objects both need to be in the same order to loop throught together
 let imageArray = ["archer.png", "brothers.png", "caveMan.png", "elephant.png", "iceland.png"];
-let captionArray = ["robin hood", "brothers in arms", "ogre", "elephant", "iceland"];
+let captionArray = ["robin hood", "brothers in arms", "flinstone", "elephant", "iceland"];
 let picture = [];
 
 //definition of buttons 
@@ -9,6 +9,9 @@ let leftClick = document.getElementById("buttonLeft");
 let rightClick = document.getElementById("buttonRight");
 let play = document.getElementById("playButton");
 
+//definition of caption and the main image container
+let mainContainer = document.querySelector("#carouselContainer");
+let imageFrame = document.querySelector(".imageFrame");
 let imgCaption = document.querySelector(".caption");
 
 // track number of the image within the gallery
@@ -33,33 +36,68 @@ for (let i = 0; i < imageArray.length; i++){
 //switch on first picture in the gallery
 picture[n].classList.replace('imgOFF', 'imgON');
 
+//flip animation of the carousel
+function flipAnimation() {
+  mainContainer.classList.toggle("rotate"); 
+}
+
 //slide forward
-function slideForward(){
-    if(n === picture.length-1){
-        n = 0;   
-        picture[picture.length-1].classList.replace("imgON", "imgOFF");        
-        picture[n].classList.replace("imgOFF", "imgON"); 
-    } else {
-        picture[n+1].classList.replace('imgOFF', 'imgON');
-        picture[n].classList.replace('imgON', 'imgOFF');
-        n++;  
-    }
-        imgCaption.innerHTML = captionArray[n];         
+function switchForward(){ 
+
+  imageFrame.classList.toggle("back"); //flip frame to position buttons caption from mirrored position
+  if(n === picture.length-1){
+  n = 0;   
+  picture[picture.length-1].classList.replace("imgON", "imgOFF");       
+  picture[n].classList.replace("imgOFF", "imgON");
+
+} else {
+  picture[n+1].classList.replace('imgOFF', 'imgON');
+  picture[n].classList.replace('imgON', 'imgOFF');
+  n++;  
+}
+  imgCaption.innerHTML = captionArray[n];  
+  
+ // reset timer for slide show 
+  if (alt === 1){
+    clearInterval(slideRate);
+    slideRate = setInterval(slideForward, 5000);
+  }
 }
 
 //slide backwards
-function slideBack(){
+function switchBack(){
+  imageFrame.classList.toggle("back"); //flip frame to position buttons caption from mirrored position
+
     if(n === 0){
         n = picture.length - 1;
-        picture[n].classList.replace('imgOFF', 'imgON');
         picture[0].classList.replace('imgON', 'imgOFF');
+        picture[n].classList.replace('imgOFF', 'imgON');
     } else {
-        picture[n-1].classList.replace('imgOFF', 'imgON');
         picture[n].classList.replace('imgON', 'imgOFF');
+        picture[n-1].classList.replace('imgOFF', 'imgON');
         n--;
     }
+
     imgCaption.innerHTML = captionArray[n];
-} 
+
+// reset timer for slide show
+    if (alt === 1){
+      clearInterval(slideRate);
+      slideRate = setInterval(slideForward, 5000);
+    }
+}
+
+//flip slide animations
+function slideForward(){
+  flipAnimation();
+  setTimeout(switchForward, 1200); 
+}
+
+function slideBack(){
+  flipAnimation();
+  setTimeout(switchBack, 1200); 
+}
+
 
 //slide carousel on buttom clicks
 rightClick.addEventListener("click", slideForward);
@@ -78,40 +116,32 @@ document.addEventListener("keyup", function(e){
 let slideRate;
 // initiation of auto slide show after whole page is loaded
 window.addEventListener("load", function(e){
-    slideRate = setInterval(slideForward, 2000); //3000 miliseconds is a 3 second delay between slides
+    slideRate = setInterval(slideForward, 5000); //2000 miliseconds are 2 second delay between slides
     play.innerHTML = ">";
 });
 
-// start slide show
-   function startSlide(){
-       slideRate = setInterval(slideForward, 2000);
-    }
-// stops automatic slide show
-  function stopSlide(){
-        clearInterval(slideRate);  
-  }
-// start/stop slide show
+// start/stop slide show by clicking on the play button
   play.addEventListener("click", function(e){
       if (alt === 1){
-        stopSlide(); 
+        clearInterval(slideRate);  
         play.innerHTML = "II";
         alt = 0;
       } else if (alt === 0){
-        startSlide();
+        slideRate = setInterval(slideForward, 5000);
         play.innerHTML = ">";
         alt = 1;
       }   
     });
-
+// start/stop slide show using enter key
     document.addEventListener("keyup", function(e){
         let key = e.keyCode;
         if (key === 13){
         if (alt === 1){
-          stopSlide();
+      clearInterval(slideRate);  
           play.innerHTML = "II"; 
           alt = 0;
         } else if (alt === 0){
-          startSlide();
+      slideRate = setInterval(slideForward, 5000);   
           play.innerHTML = ">";
           alt = 1;
         }  
@@ -121,4 +151,3 @@ window.addEventListener("load", function(e){
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~end of the carousel function~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
